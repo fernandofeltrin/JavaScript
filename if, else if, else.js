d6f720f5863840aa4bda197b63c66d373ae26ebc
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-10-07 16:42:09 -0300

    Create if, else if, else.js

---
+++ if, else if, else.js
@@ -0,0 +1,9 @@
+let nota = 100
+
+if (nota == 100) {
+      console.log('Aprovado com nota máxima!!!')
+} else if (nota >= 70 && nota <= 99) {
+    console.log('Aprovado por média!!!')
+} else {
+    console.log('Reprovado!!!')
+}
