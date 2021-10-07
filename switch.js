16a99ffd80a706aeb7931f33d07877f07b7a1dfd
Fernando Belomé Feltrin <fernando2rad@gmail.com>
2021-10-07 17:05:23 -0300

    Create switch.js

---
+++ switch.js
@@ -0,0 +1,19 @@
+let nota = 4
+
+switch (nota) {
+    case 10:
+        console.log('Aprovado com nota máxima!!!')
+        break
+    case 9:
+    case 8:
+    case 7:
+        console.log('Aprovado por média!!!')
+        break
+    case 6:
+    case 5:
+    case 4:
+    case 3:
+    case 2:
+    case 1:
+        console.log('Reprovado!!!')
+}
